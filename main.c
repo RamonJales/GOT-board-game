c7d730955eee33cea1ac56e35758c6113b93911c
RamonJales <ramonjales123@gmail.com>
2022-08-23 21:07:56 -0300

    Jhon snow piece implemented on board

--- main.c
+++ main.c
@@ -4,6 +4,9 @@ void boardInit(char scenery[10][10], int wPosition[2]) {
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
             if (i == wPosition[0] && j == wPosition[1]) {
+                scenery[i][j] = 'J';
+            }
+            else if (i == 0 && j == 0) {
                 scenery[i][j] = 'W';
             }
             else if (i == 4 && j == 4) {
